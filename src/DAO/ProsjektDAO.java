ca99614584d8f4876eef92fbc5b14dd32b05e71c
584973 <54097912+584973@users.noreply.github.com>
2021-03-11 18:45:37 +0100

    prosjekttingetang

---
+++ src/DAO/ProsjektDAO.java
@@ -0,0 +1,27 @@
+package DAO;
+
+
+import java.util.List;
+
+import entiteter.Prosjekt;
+
+public class ProsjektDAO {
+
+	public List<Prosjekt> getProsjekter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Prosjekt getProsjekt(String prosjektID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void oppdater(Prosjekt pros) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//TODO her skal det inn database greier for prosjekt.
+
+}
--- src/servlett/AlternativStemmeServlet.java
+++ src/servlett/AlternativStemmeServlet.java
@@ -1,50 +1,61 @@
 package servlett;
 
 import java.io.IOException;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import hjelpeKlasser.PoengHjelp;
+import DAO.ProsjektDAO;
+import entiteter.Prosjekt;
 
-/**
- * Servlet implementation class AlternativStemmeServlet
- */
 @WebServlet("/AlternativStemmeServlet")
 public class AlternativStemmeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    PoengHjelp hjelp = new PoengHjelp();
-	
-	
-    public AlternativStemmeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		request.getRequestDispatcher("WEB-INF/AlternativStemmeKvittering.jsp").forward(request, response);
-		
-		
+	@EJB
+	ProsjektDAO phjelp = new ProsjektDAO();
+
+	public AlternativStemmeServlet() {
+		super();
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String ratingVerdi = request.getParameter("rating");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.getRequestDispatcher("WEB-INF/AlternativStemmeKvittering.jsp").forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String feilmelding = "";
+
 		String mobilNr = request.getParameter("tlf");
-		
-		// her skal prosessering av stemmen skje
-		
+		String ratingVerdiStr = request.getParameter("rating");
+		float ratingVerdi = Float.parseFloat(ratingVerdiStr);
+
+		String prosjektID = request.getParameter("prosjektID"); // denne skal komme fra en hidden i jsp formen
+
+		Prosjekt pros = phjelp.getProsjekt(prosjektID);
+
+		// Om det skulle skje et problem og prosjektID ikke er valid
+		if (pros == null) {
+			feilmelding = "Prosjektet du prøver stemme på eksisterer ikke.";
+			request.getSession().setAttribute("feil", feilmelding);
+		}
+
+		// Nyeste stemme gjelder, så gamle slettes først.
+		pros.getStemmer().remove(mobilNr);
+		pros.getStemmer().put(mobilNr, ratingVerdi);
+
+		// Trenger en måte å oppdatere prosjektet og dens stemmer i databasen.
+		phjelp.oppdater(pros);
+
 		response.sendRedirect("AlternativStemmeServlet");
 	}
 

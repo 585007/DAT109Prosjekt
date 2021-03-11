951482ca8b12c3ec981e64a131ccf23b275ace58
584973 <54097912+584973@users.noreply.github.com>
2021-03-11 11:36:54 +0100

    Revert "prosjekt"
    This reverts commit 61ef891c5221877fa9392d31a42d1dc1652f5058.

--- src/hjelpeKlasser/PoengHjelpTest.java
+++ src/hjelpeKlasser/PoengHjelpTest.java
@@ -1,12 +1,10 @@
-package hjelpeKlasseTester;
+package hjelpeKlasser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
-
-import hjelpeKlasser.PoengHjelp;
 
 class PoengHjelpTest {
 

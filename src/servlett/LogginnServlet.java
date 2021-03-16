9ba59b0f2d173e1e97672a9de6c0f42243553c9d
H116914 <54101027+H116914@users.noreply.github.com>
2021-03-16 22:37:26 +0100

    Revert "Merge branch 'main' of https://github.com/585007/DAT109Prosjekt.git into"
    This reverts commit 512601ed611d82f86b7c4a5348704c22fbb3bbe2, reversing
    changes made to 12e70d42a698fcdbeab2c4a57be00cb6ecb1f132.

--- src/servlett/LogginnServlet.java
+++ src/servlett/LogginnServlet.java
@@ -10,36 +10,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-//import DAO.AdmDAO;
-//import hjelpeKlasser.PassordHjelp;
+import DAO.AdmDAO;
+import hjelpeKlasser.PassordHjelp;
 
 /**
  * Innlogging med verifisering av brukernavn og passord
  * 
  * @author Svein Ove Surdal
- *name = "LogginnServlet", urlPatterns = 
+ *
  */
-@WebServlet("/logginn")
+@WebServlet(name = "logginn", urlPatterns = "/logginn")
 public class LogginnServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-<<<<<<< HEAD
 	// @EJB
 	// AdmDAO admDAO;
 
-=======
-	//@EJB
-	//AdmDAO admDAO;
-	
-@Override
->>>>>>> branch 'main' of https://github.com/585007/DAT109Prosjekt.git
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		request.getRequestDispatcher("WEB-INF/logginn.jsp").forward(request, response);
 	}
 
-@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
@@ -49,7 +41,6 @@ public class LogginnServlet extends HttpServlet {
 		}
 		sesjon = request.getSession(true);
 
-<<<<<<< HEAD
 		String passord = request.getParameter("passord");
 		String bruker = request.getParameter("bruker");
 		
@@ -59,15 +50,6 @@ public class LogginnServlet extends HttpServlet {
 		String passordHardKodet = "pass";
 		String brukerHardKodet = "adm";
 		if (!bruker.equals(brukerHardKodet) || !passord.equals(passordHardKodet)) { // til bruk under test med hardkoding av passord og brukernavn
-=======
-//		String passord = request.getParameter("passord");
-//		String bruker = request.getParameter("bruker");
-
-//		if (!bruker.equals(admDAO.hentBrukernavn())
-//				|| PassordHjelp.validerMedSalt(passord, admDAO.hentSalt() , admDAO.hentPassord())) {
-			
-		if(false) {
->>>>>>> branch 'main' of https://github.com/585007/DAT109Prosjekt.git
 			String loginMessage = "Ugyldig brukernavn og/eller passord";
 			request.setAttribute("loginMessage", loginMessage);
 			request.getRequestDispatcher("WEB-INF/logginn.jsp").forward(request, response);

a5220925f7c9be0ab2175c5e2e895dced34fbaf8
Hav0r <Hav0r@DESKTOP-L1NQTMS>
2021-03-22 23:21:03 +0100

    deployable(???)

---
+++ src/servlett/StemmeKvitteringServlet.java
@@ -0,0 +1,40 @@
+package servlett;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StemmeKvitteringServlet
+ */
+@WebServlet("/stemmekvittering")
+public class StemmeKvitteringServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StemmeKvitteringServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.getRequestDispatcher("WEB-INF/StemmeKvittering.jsp").forward(request, response);}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

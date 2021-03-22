156a96c2b7495ee5a50fb120fdbcf652f5f04f22
585007 <585007@stud.hvl.no>
2021-03-22 21:18:06 +0100

    SletteServlet fungerer nå

--- WebContent/WEB-INF/SlettProsjekt.jsp
+++ WebContent/WEB-INF/SlettProsjekt.jsp
@@ -6,18 +6,18 @@
 <meta charset="ISO-8859-1">
 <link rel="stylesheet"
 	href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
-<title>Admin - legg til prosjekt</title>
+<title>Admin - slett prosjekt</title>
 </head>
 <body>
-	<form action="LeggTilProsjekt" method="post"
+	<form action="slettProsjekt" method="post" 
 		class="pure-form pure-form-aligned">
-		<fieldset>
+	<fieldset>
 			<div class="pure-control-group">
-				<label for="navn">Navn:</label> <input type="text" name="navn" />
+				<label for="navn">ProsjektNr:</label> 
+				<input type="text" name="prosjektNr" />
 			</div>
 			<div class="pure-control-group">
-				<button type="submit" class="pure-button pure-button-primary">Legg
-					til</button>
+				<button type="submit" class="pure-button pure-button-primary">Slett</button>
 			</div>
 		</fieldset>
 	</form>
---
+++ src/servlett/SlettProsjektServlet.java
@@ -0,0 +1,75 @@
+package servlett;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.ProsjektDAO;
+import DAO.StemmeDAO;
+import entiteter.Prosjekt;
+import hjelpeKlasser.GyldigSesjon;
+
+/**
+ * Servlet implementation class SletteServlet
+ */
+
+/**
+ * 
+ * @author ??? / Ruben Aadland
+ *	
+ * Sletter prosjektet og stemmene som prosjektet hadde
+ * Sender brukeren tilbake til adminsiden
+ */
+
+@WebServlet(name = "SlettProsjektServlet", urlPatterns = "/slettProsjekt")
+public class SlettProsjektServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@EJB
+	StemmeDAO stemmeDAO;
+	@EJB
+	ProsjektDAO prosjektDAO;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sesjon = request.getSession(false);
+		
+		if(GyldigSesjon.innlogget(sesjon)) {
+			request.getRequestDispatcher("WEB-INF/SlettProsjekt.jsp").forward(request, response);
+		}else {
+			response.sendRedirect("logginn");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sesjon = request.getSession(false);
+		
+		if(GyldigSesjon.innlogget(sesjon)) {
+			String prosjektNr = request.getParameter("prosjektNr");
+			
+			if(prosjektNr != null) {
+				prosjektDAO.slettProsjekt(prosjektNr);
+				
+				stemmeDAO.fjernStemmerForProsjekt(prosjektNr);
+				
+				response.sendRedirect("admin");
+			}else {
+				response.sendRedirect("slettProsjekt");
+			}
+		}else {
+			response.sendRedirect("logginn");
+		}
+	}
+
+}

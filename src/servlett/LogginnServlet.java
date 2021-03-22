878419a9ca0b42583b9acc5a1a6e7ff45f267f4e
H116914 <54101027+H116914@users.noreply.github.com>
2021-03-22 16:19:28 +0100

    s

--- src/servlett/LogginnServlet.java
+++ src/servlett/LogginnServlet.java
@@ -2,6 +2,7 @@ package servlett;
 
 import java.io.IOException;
 
+import javax.ejb.EJB;
 //import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import DAO.StemmeDAO;
 
 //import DAO.AdmDAO;
 //import hjelpeKlasser.PassordHjelp;
@@ -23,11 +26,13 @@ import javax.servlet.http.HttpSession;
 public class LogginnServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	// @EJB
+	@EJB
+	StemmeDAO stemmeDAO = new StemmeDAO(); // tester ut stemme DB
 	// AdmDAO admDAO;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		stemmeDAO.hentAlleStemmer(); // tester ut stemme DB
 		request.getRequestDispatcher("WEB-INF/logginn.jsp").forward(request, response);
 	}
 

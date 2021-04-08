4e0244385f02fd3c7211c250f910e7d6939bd99a
Hav0r <Hav0r@DESKTOP-L1NQTMS>
2021-04-08 19:43:18 +0200

    gsd

--- src/DAO/DBconfig.java
+++ src/DAO/DBconfig.java
@@ -1,5 +1,9 @@
 package DAO;
-
+/**
+ * 
+ * @author Ruben 
+ * inneholder navnet på schemaet som skal bli brukt
+ */
 public class DBconfig {
 	//Standard skjema for DB
 	public final static String schema = "expo";
